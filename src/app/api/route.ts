360a9ba90d203bacd629008ed75275e4ff586281
ZinHt9tHlaing <phyoearkar267@gmail.com>
2025-12-05 22:02:30 +0630

    refactor and upgrade the whole project

--- src/app/api/route.ts
+++
@@ -1,7 +0,0 @@
-export const dynamic = "force-static";
-
-export async function GET() {
-  const posts = [{ id: 1, title: "hello", body: "world" }];
-
-  return Response.json({ posts });
-}
--- src/server/action.ts
+++
@@ -1,9 +0,0 @@
-"use server";
-
-import { PrismaClient } from "../../generated/prisma";
-
-const prisma = new PrismaClient();
-
-export const readData = async () => {
-  const todo = await prisma.todo.findMany();
-};
